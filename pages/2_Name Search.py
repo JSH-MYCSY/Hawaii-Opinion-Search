d4407865844beb7432676b4a695abe685610d7d9
JSH-MYCSY <caseyjos@hawaii.edu>
2024-04-26 00:30:48 -1000

    fixed minor error

--- pages/2_Name Search.py
+++ pages/2_Name Search.py
@@ -9,7 +9,7 @@ if st.button("Name Search"):
     new_list = nameSearch(user_text)
     df1 = pd.DataFrame({
         "Case Name": [sublist[0] for sublist in new_list],
-        "Case Date": [sublist[2][0:9] for sublist in new_list],
+        "Case Date": [sublist[2][0:10] for sublist in new_list],
         "Case Url": [sublist[1] for sublist in new_list]
     })
     st.data_editor(df1, column_config={"Case Url": st.column_config.LinkColumn(label="Case Url", display_text="Case Link")}, hide_index=True)
