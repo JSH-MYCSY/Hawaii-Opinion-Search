64b30adc177e189626347006252e544d62b0bf3a
JSH-MYCSY <caseyjos@hawaii.edu>
2024-04-26 00:30:04 -1000

    got rid of index in dataframe

--- pages/2_Name Search.py
+++ pages/2_Name Search.py
@@ -9,10 +9,10 @@ if st.button("Name Search"):
     new_list = nameSearch(user_text)
     df1 = pd.DataFrame({
         "Case Name": [sublist[0] for sublist in new_list],
-        "Case Date": [sublist[2] for sublist in new_list],
+        "Case Date": [sublist[2][0:9] for sublist in new_list],
         "Case Url": [sublist[1] for sublist in new_list]
     })
-    st.data_editor(df1, column_config={"Case Url": st.column_config.LinkColumn(label="Case Url", display_text="Case Link")})
+    st.data_editor(df1, column_config={"Case Url": st.column_config.LinkColumn(label="Case Url", display_text="Case Link")}, hide_index=True)
     # for value, item in enumerate(new_list):
     #     st.write(str(item[0]))
     #     st.link_button("Name Link " + str(value), item[1])
