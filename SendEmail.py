8787346be34671aa7ce7564921b515dbe5639b77
JSH-MYCSY <caseyjos@hawaii.edu>
2024-05-03 00:53:37 -1000

    Update SendEmail.py

--- SendEmail.py
+++ SendEmail.py
@@ -14,7 +14,7 @@ with open("CourtOpinion_Hawaii_New.csv", "r", encoding="utf-8") as file:
     read = csv.reader(file)
     for row in read:
         try: # this tries to pull out only the cases that were added in the past year.
-            if(datetime.strptime(row[2],'%Y-%m-%d %H:%M:%S') > (datetime.now(tz=timezone(-timedelta(hours=10))) - timedelta(days=6))):
+            if(datetime.strptime(row[2][0:10],'%Y-%m-%d') > (datetime.today() - timedelta(days=6))):
                 titleTemp = str(row[1]).split("/")[-1]
                 title = titleTemp.split(".")[0]
                 with open("courtOpinionText/" + title + ".txt", "r", encoding="utf-8") as txtObj:
